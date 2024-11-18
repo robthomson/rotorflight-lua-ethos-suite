bda68aeab458943bd519a42451ff7bd450c9208f
Rob Thomson <rob@marotori.com>
2024-11-18 09:22:36 +0000

    Improve esc detection by waiting for at least 2 bytes to arrive (#38)

--- scripts/rfsuite/app/pages/esc_tool.lua
+++ scripts/rfsuite/app/pages/esc_tool.lua
@@ -31,7 +31,7 @@ local function getESCDetails()
         command = 217, -- MSP_STATUS
         processReply = function(self, buf)
 
-            if buf[1] == mspSignature then
+            if #buf >=2 and buf[1] == mspSignature then
 
                 escDetails.model = ESC.getEscModel(buf)
                 escDetails.version = ESC.getEscVersion(buf)
@@ -213,6 +213,10 @@ local function openPage(pidx, title, script)
 end
 
 local function wakeup()
+
+    if foundESC == false and rfsuite.bg.msp.mspQueue:isProcessed() then
+            getESCDetails()
+    end
 
     -- enable the form
     if foundESC == true and foundESCupdateTag == false then
