d9597ac72c410e0b407d370a20fd1ba8978d84bd
Rob Thomson <rob@marotori.com>
2025-01-01 09:50:56 +0000

    Update governor.lua (#94)

--- scripts/rfsuite/widgets/governor/governor.lua
+++ scripts/rfsuite/widgets/governor/governor.lua
@@ -1,7 +1,5 @@
 --[[
-
  * Copyright (C) Rotorflight Project
- *
  *
  * License GPLv3: https://www.gnu.org/licenses/gpl-3.0.en.html
  *
@@ -11,143 +9,109 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- 
- * Note.  Some icons have been sourced from https://www.flaticon.com/
- * 
-
+ *
+ * Note: Some icons have been sourced from https://www.flaticon.com/
 ]]--
+
 --[[
 set crsf_flight_mode_reuse = GOV_ADJFUNC
-]] --
-local rf2gov = {}
+]]--
 
-rf2gov.refresh = true
-rf2gov.environment = system.getVersion()
-rf2gov.oldsensors = {"govmode"}
-rf2gov.wakeupSchedulerUI = os.clock()
+local rf2gov = {
+    refresh = true,
+    environment = system.getVersion(),
+    oldsensors = { govmode = "" },
+    wakeupSchedulerUI = os.clock(),
+}
 
-local governorMap = {}
-governorMap[0] = "OFF"
-governorMap[1] = "IDLE"
-governorMap[2] = "SPOOLUP"
-governorMap[3] = "RECOVERY"
-governorMap[4] = "ACTIVE"
-governorMap[5] = "THR-OFF"
-governorMap[6] = "LOST-HS"
-governorMap[7] = "AUTOROT"
-governorMap[8] = "BAILOUT"
-governorMap[100] = "DISABLED"
-governorMap[101] = "DISARMED"
+local governorMap = {
+    [0] = "OFF",
+    [1] = "IDLE",
+    [2] = "SPOOLUP",
+    [3] = "RECOVERY",
+    [4] = "ACTIVE",
+    [5] = "THR-OFF",
+    [6] = "LOST-HS",
+    [7] = "AUTOROT",
+    [8] = "BAILOUT",
+    [100] = "DISABLED",
+    [101] = "DISARMED",
+}
 
 local sensors
 
-function rf2gov.sensorMakeNumber(x)
-    if x == nil or x == "" then x = 0 end
-
-    x = string.gsub(x, "%D+", "")
-    x = tonumber(x)
-    if x == nil or x == "" then x = 0 end
-
-    return x
+-- Helper function to convert a value to a valid number
+function rf2gov.sensorMakeNumber(value)
+    value = value or 0
+    local num = tonumber(string.gsub(tostring(value), "%D+", ""))
+    return num or 0
 end
 
 function rf2gov.create(widget)
-    return
+    -- Placeholder for widget creation logic
 end
 
 function rf2gov.paint(widget)
-
     local w, h = lcd.getWindowSize()
-
     lcd.font(FONT_XXL)
 
-    if rfsuite.bg.active() then
-        if sensors then
-            str = sensors.govmode
-        else
-            str = ""
-        end
-    else
-        str = "BG TASK DISABLED"
-    end
-    tsizeW, tsizeH = lcd.getTextSize(str)
+    local str = rfsuite.bg.active() and (sensors and sensors.govmode or "") or "BG TASK DISABLED"
+    local tsizeW, tsizeH = lcd.getTextSize(str)
 
-    offsetY = 5
-
-    posX = w / 2 - tsizeW / 2
-    posY = h / 2 - tsizeH / 2 + offsetY
+    local posX = (w - tsizeW) / 2
+    local posY = (h - tsizeH) / 2 + 5
 
     lcd.drawText(posX, posY, str)
-
 end
 
 function rf2gov.getSensors()
+    if not rfsuite.bg.active() then return end
 
-    if rfsuite.bg.active() == false then return end
+    local govmode = ""
 
-    if rf2gov.environment.simulation == true then
+    if rf2gov.environment.simulation then
         govmode = "DISABLED"
     else
-
-        govSOURCE = rfsuite.bg.telemetry.getSensorSource("governor")
-
+        local govSOURCE = rfsuite.bg.telemetry.getSensorSource("governor")
+        
         if rfsuite.bg.telemetry.getSensorProtocol() == 'lcrsf' then
-            if govSOURCE ~= nil then govmode = govSOURCE:stringValue() end
+            govmode = govSOURCE and govSOURCE:stringValue() or ""
         else
-
-            if govSOURCE ~= nil then
-                govId = govSOURCE:value()
-
-                if governorMap[govId] == nil then
-                    govmode = "UNKNOWN"
-                else
-                    govmode = governorMap[govId]
-                end
-
-            else
-                govmode = ""
-            end
-
+            local govId = govSOURCE and govSOURCE:value()
+            govmode = governorMap[govId] or (govId and "UNKNOWN" or "")
         end
-
     end
 
-    if rf2gov.oldsensors.govmode ~= govmode then rf2gov.refresh = true end
+    if rf2gov.oldsensors.govmode ~= govmode then
+        rf2gov.refresh = true
+    end
 
-    ret = {govmode = govmode}
-    rf2gov.oldsensors = ret
+    sensors = { govmode = govmode }
+    rf2gov.oldsensors = sensors
 
-    return ret
+    return sensors
 end
 
--- MAIN WAKEUP FUNCTION. THIS SIMPLY FARMS OUT AT DIFFERING SCHEDULES TO SUB FUNCTIONS
+-- Main wakeup function
 function rf2gov.wakeup(widget)
-
-    local schedulerUI
-    if lcd.isVisible() then
-        schedulerUI = 0.25
-    else
-        schedulerUI = 1
-    end
-
-    -- keep cpu load down by running UI at reduced interval
+    local schedulerUI = lcd.isVisible() and 0.25 or 1
     local now = os.clock()
+
     if (now - rf2gov.wakeupSchedulerUI) >= schedulerUI then
         rf2gov.wakeupSchedulerUI = now
         rf2gov.wakeupUI()
     end
-
 end
 
-function rf2gov.wakeupUI(widget)
+function rf2gov.wakeupUI()
     rf2gov.refresh = false
-    sensors = rf2gov.getSensors()
+    rf2gov.getSensors()
 
-    if rf2gov.refresh == true then lcd.invalidate() end
-
-    return
+    if rf2gov.refresh then
+        lcd.invalidate()
+    end
 end
 
 return rf2gov
