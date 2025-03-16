3d20a090c003244c6cf00c70665fafc38dd6739f
Rob Thomson <rob@marotori.com>
2025-03-16 17:26:53 +0000

    Create es.lua (#429)

---
+++ scripts/rfsuite/i18n/telemetry/es.lua
@@ -0,0 +1,42 @@
+--[[
+
+ * Copyright (C) Rotorflight Project
+ *
+ *
+ * License GPLv3: https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * Note.  Some icons have been sourced from https://www.flaticon.com/
+ * 
+
+]] --
+
+local en = {
+    sensors = {
+        rssi         = "RSSI",
+        arming_flags = "Banderas Armado",
+        voltage      = "Voltaje",
+        headspeed    = "Veloc. Rotor",
+        current      = "Corriente",
+        esc_temp     = "Temperatura ESC",
+        mcu_temp     = "Temperatura MCU",
+        fuel         = "Nivel de Carga",
+        consumption  = "Consumo",
+        governor     = "Estado Governor",
+        adj_func     = "Ajus.(Función)",
+        adj_val      = "Ajus.(Valor)",
+        pid_profile  = "Perfil PID",
+        rate_profile = "Perfil Tasas",
+        throttle_pct = "% Acelerador",
+    }
+}
+
+return es
