1d3f262e3c84be835f7d5639cecd93112c90e75b
LENHARDT Romain <r.lenhardt69@gmail.com>
2025-03-16 09:45:42 +0100

    telemetry files pages (#425)
    * Adding Gov functions
    
    * More changes for FR
    
    * Update fr.lua
    
    * Minors changes Frenchv2
    
    * SR Explications
    
    * Gov Page+Filters
    
    * adding french telemetry page

---
+++ scripts/rfsuite/i18n/telemetry/fr.lua
@@ -0,0 +1,42 @@
+--[[
+
+ * Copyright (C) Rotorflight Project
+ *
+ *
+ * License GPLv3: https://www.gnu.org/licenses/gpl-3.0.fr.html
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * Note.  Some icons have been sourced from https://www.flaticon.com/
+ * 
+
+]] --
+
+local fr = {
+    sensors = {
+        rssi         = "Signal du Recepteur",
+        arming_flags = "Drapeaux Armements",
+        voltage      = "Tension",
+        headspeed    = "Tours Moteurs",
+        current      = "Courant",
+        esc_temp     = "Temperature ESC",
+        mcu_temp     = "Temperature Micro Processeur",
+        fuel         = "Niveau de Carburant",
+        consumption  = "Consommation",
+        governor     = "Status du Gouverneur",
+        adj_func     = "Ajustements Fonctions",
+        adj_val      = "Ajustements Valeurs",
+        pid_profile  = "Profile de PID",
+        rate_profile = "Profile de Rates",
+        throttle_pct = "Pourcentage de Gaz",
+    }
+}
+
+return fr
