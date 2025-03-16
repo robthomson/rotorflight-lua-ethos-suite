89d49a0d66c6b623bd0fcb46994329a50cec2c78
wschoech <12935270+wschoech@users.noreply.github.com>
2025-03-16 20:38:59 +0100

    429 German: Add telemetry/de.lua and app.servos.reverse translation (#432)
    Co-authored-by: Rob Thomson <rob@marotori.com>

---
+++ scripts/rfsuite/i18n/telemetry/de.lua
@@ -0,0 +1,42 @@
+--[[
+
+ * Copyright (C) Rotorflight Project
+ *
+ *
+ * License GPLv3: https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * Note.  Some icons have been sourced from https://www.flaticon.com/
+ *
+
+]] --
+
+local de = {
+    sensors = {
+        rssi         = "RSSI",
+        arming_flags = "Arming-Flags",
+        voltage      = "Spannung",
+        headspeed    = "Drehzahl",
+        current      = "Strom",
+        esc_temp     = "ESC Temperatur",
+        mcu_temp     = "MCU Temperatur",
+        fuel         = "Kraftstoffstand",
+        consumption  = "Verbrauch",
+        governor     = "Governor Status",
+        adj_func     = "Adj (Funktion)",
+        adj_val      = "Adj (Wert)",
+        pid_profile  = "PID-Profil",
+        rate_profile = "Rate-Profil",
+        throttle_pct = "Gas %",
+    }
+}
+
+return de
