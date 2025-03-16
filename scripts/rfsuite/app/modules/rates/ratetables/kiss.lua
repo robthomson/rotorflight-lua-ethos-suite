43a3e8027b429481ae24157b7be63996285482f6
rburrow87 <rob@rebrw.com>
2025-03-16 00:08:36 -0700

    More adjustments for Actual, KISS, and QuickRates (#420)
    * Fix Quick rates
    
    * Improve Actual rate defaults
    
    * More Quick rates fixes
    
    * Fix KISS rates default
    
    ---------
    
    Co-authored-by: Rob Thomson <rob@marotori.com>

--- scripts/rfsuite/app/modules/rates/ratetables/kiss.lua
+++ scripts/rfsuite/app/modules/rates/ratetables/kiss.lua
@@ -27,7 +27,7 @@ local mspapi = {
             {row = 1, col = 2, min = 0, max = 99, default = 0, decimals = 2, scale = 100, mspapi = 1, apikey = "rates_1"},
             {row = 2, col = 2, min = 0, max = 99, default = 0, decimals = 2, scale = 100, mspapi = 1, apikey = "rates_2"},
             {row = 3, col = 2, min = 0, max = 99, default = 0, decimals = 2, scale = 100, mspapi = 1, apikey = "rates_3"},
-            {row = 4, col = 2, min = 0, max = 99, default = 1, decimals = 2, scale = 100, mspapi = 1, apikey = "rates_4"},
+            {row = 4, col = 2, min = 0, max = 99, default = 0, decimals = 2, scale = 100, mspapi = 1, apikey = "rates_4"},
             -- rc curve
             {row = 1, col = 3, min = 0, max = 100, decimals = 2, scale = 100, default = 0, mspapi = 1, apikey = "rcExpo_1"},
             {row = 2, col = 3, min = 0, max = 100, decimals = 2, scale = 100, default = 0, mspapi = 1, apikey = "rcExpo_2"},
