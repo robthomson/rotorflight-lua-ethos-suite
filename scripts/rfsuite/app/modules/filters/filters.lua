b23411f991e50f095f242cd2268b4e24a9b6dbf5
Rob Thomson <rob@marotori.com>
2025-03-17 10:18:29 +0000

    Additional Filtering Params (#435)
    * Filtering
    
    * Update filters.lua
    
    * updates

--- scripts/rfsuite/app/modules/filters/filters.lua
+++ scripts/rfsuite/app/modules/filters/filters.lua
@@ -11,7 +11,11 @@ local mspapi = {
             {t = rfsuite.i18n.get("app.modules.filters.lowpass_2"),     label = 5, inline_size = 40.15},
             {t = "",                                                    label = 6, inline_size = 40.15},
             {t = rfsuite.i18n.get("app.modules.filters.notch_1"),       label = 7, inline_size = 13.6},
-            {t = rfsuite.i18n.get("app.modules.filters.notch_2"),       label = 8, inline_size = 13.6}
+            {t = rfsuite.i18n.get("app.modules.filters.notch_2"),       label = 8, inline_size = 13.6},
+            {t = rfsuite.i18n.get("app.modules.filters.dyn_notch"),     label = 9, inline_size = 13.6},
+            {t = "",                                                    label = 10, inline_size = 13.6},
+            {t = rfsuite.i18n.get("app.modules.filters.rpm_filter"),    label = 11, inline_size = 40.15},
+            {t = "",                                                    label = 12, inline_size = 40.15},
         },
         fields = {
             {t = rfsuite.i18n.get("app.modules.filters.filter_type"), label = 1, inline = 1, mspapi = 1, apikey = "gyro_lpf1_type", type = 1},
@@ -23,7 +27,13 @@ local mspapi = {
             {t = rfsuite.i18n.get("app.modules.filters.center"),      label = 7, inline = 2, mspapi = 1, apikey = "gyro_soft_notch_hz_1"},
             {t = rfsuite.i18n.get("app.modules.filters.cutoff"),      label = 7, inline = 1, mspapi = 1, apikey = "gyro_soft_notch_cutoff_1"},
             {t = rfsuite.i18n.get("app.modules.filters.center"),      label = 8, inline = 2, mspapi = 1, apikey = "gyro_soft_notch_hz_2"},
-            {t = rfsuite.i18n.get("app.modules.filters.cutoff"),      label = 8, inline = 1, mspapi = 1, apikey = "gyro_soft_notch_cutoff_2"}
+            {t = rfsuite.i18n.get("app.modules.filters.cutoff"),      label = 8, inline = 1, mspapi = 1, apikey = "gyro_soft_notch_cutoff_2"},
+            {t = rfsuite.i18n.get("app.modules.filters.notch_c"),     label = 9, inline = 2, mspapi = 1, apikey = "dyn_notch_count"},
+            {t = rfsuite.i18n.get("app.modules.filters.notch_q"),     label = 9, inline = 1, mspapi = 1, apikey = "dyn_notch_q"},
+            {t = rfsuite.i18n.get("app.modules.filters.notch_min_hz"),label = 10, inline = 2, mspapi = 1, apikey = "dyn_notch_min_hz"},
+            {t = rfsuite.i18n.get("app.modules.filters.notch_max_hz"),label = 10, inline = 1, mspapi = 1, apikey = "dyn_notch_max_hz"},
+            {t = rfsuite.i18n.get("app.modules.filters.rpm_preset"),  label = 11, inline = 1, mspapi = 1, apikey = "rpm_preset", type = 1, apiversiongte = 12.08},
+            {t = rfsuite.i18n.get("app.modules.filters.rpm_min_hz"),  label = 12, inline = 1, mspapi = 1, apikey = "rpm_min_hz", apiversiongte = 12.08},
         }
     }                 
 }
