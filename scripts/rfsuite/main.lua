f20bf6ba5c2c75304a25aeadeace1401ec59b937
Rob Thomson <rob@marotori.com>
2025-03-18 17:42:23 +0000

    Update main.lua (#449)

--- scripts/rfsuite/main.lua
+++ scripts/rfsuite/main.lua
@@ -34,7 +34,7 @@ config.supportedMspApiVersion = {"12.06", "12.07","12.08"}          -- supported
 config.simulatorApiVersionResponse = {0, 12, 8}                     -- version of api return by simulator
 config.logLevel= "info"                                             -- off | info | debug [default = info]
 config.logToFile = false                                            -- log to file [default = false] (log file is in /scripts/rfsuite/logs)
-config.logMSP = true                                               -- log msp messages [default =  false]
+config.logMSP = false                                               -- log msp messages [default =  false]
 config.logMemoryUsage = false                                       -- log memory usage [default = false]
 config.developerMode = false                                        -- show developer tools on main menu [default = false]
 
