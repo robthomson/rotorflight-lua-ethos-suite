aec06fd505204909db4039d596f4de4c6064fc79
Rob Thomson <rob@marotori.com>
2025-03-23 22:33:46 +0000

    Move demo to correct folder (#486)

--- docs/rfsuite-api-demo/main.lua
+++ docs/rfsuite-api-demo/main.lua
@@ -1,124 +1,106 @@
 --[[
-
  * Copyright (C) Rotorflight Project
- *
- *
  * License GPLv3: https://www.gnu.org/licenses/gpl-3.0.en.html
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
- *
+ * it under the terms of the GNU General Public License version 3.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- 
- * Note.  Some icons have been sourced from https://www.flaticon.com/
- * 
- 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Note: Some icons have been sourced from https://www.flaticon.com/
+ *
  * This script is a simple widget that shows how you can access various
- * session variables from rotorflight.
+ * session variables from Rotorflight.
+]]--
 
-]] --
- 
 local environment = system.getVersion()
-
-local lastPrintTime = 0  -- Store the last time the debug was rfsuite.utils.loged
-local printInterval = 5   -- Interval in seconds
+local lastPrintTime = 0
+local printInterval = 5
 
 local apiValue = nil
 
 local function create()
-    -- this is where we create the widget
-	local widget = {}
-    return widget 
+    -- Create the widget
+    local widget = {}
+    return widget
 end
 
 local function configure(widget)
-    -- this is where we configure the widget
+    -- Configure the widget (called by Ethos forms)
 end
 
 local function paint(widget)
-    -- this is where we paint the widget
+    -- Paint the widget (on screen)
 end
 
 local function wakeup(widget)
-    -- this is where we handle the main loop
-
-    local currentTime = os.clock() -- Get the current time in seconds
+    -- Handle the main loop
+    local currentTime = os.clock()
 
     if currentTime - lastPrintTime >= printInterval then
         if rfsuite and rfsuite.tasks.active() then
+            -- Log Rotorflight session information
+            rfsuite.utils.log("Craft Name: " .. (rfsuite.session.craftName or "-"), "info")
+            rfsuite.utils.log("Model Id: " .. (rfsuite.session.modelID or "-"), "info")
+            rfsuite.utils.log("API Version: " .. (rfsuite.session.apiVersion or "-"), "info")
+            rfsuite.utils.log("Tail Mode: " .. (rfsuite.session.tailMode or "-"), "info")
+            rfsuite.utils.log("Swash Mode: " .. (rfsuite.session.swashMode or "-"), "info")
+            rfsuite.utils.log("Servo Count: " .. (rfsuite.session.servoCount or "-"), "info")
+            rfsuite.utils.log("Governor Mode: " .. (rfsuite.session.governorMode or "-"), "info")
 
-            -- We have a number of session vars you can access
-            -- These are set by the rotorflight system and are available to you
-            rfsuite.utils.log("Craft Name: " .. (rfsuite.session.craftName or "-"),"info")
-            rfsuite.utils.log("Model Id: " .. (rfsuite.session.modelID or "-"),"info")
-			rfsuite.utils.log("API Version: " .. (rfsuite.session.apiVersion or "-"),"info")
-			rfsuite.utils.log("Tail Mode: " .. (rfsuite.session.tailMode or "-"),"info")
-			rfsuite.utils.log("Swash Mode: " .. (rfsuite.session.swashMode or "-"),"info")
-			rfsuite.utils.log("Servo count: " .. (rfsuite.session.servoCount or "-"),"info")
-			rfsuite.utils.log("Governor mode: " .. (rfsuite.session.governorMode or "-"),"info")
-
-            -- Get a sensor source regardless of protocol
-            -- You can see sensor names in rfsuite/tasks/telemetry/telemetry.lua 
-            -- Look at the sensorTable
+            -- Read telemetry sensors
             local armflags = rfsuite.tasks.telemetry.getSensorSource("armflags")
-            rfsuite.utils.log("Arm Flags: " .. (armflags:value() or "-"),"info")
+            rfsuite.utils.log("Arm Flags: " .. (armflags:value() or "-"), "info")
 
             local rpm = rfsuite.tasks.telemetry.getSensorSource("rpm")
-            rfsuite.utils.log("Headspeed: " .. (rpm:value() or "-"),"info")
+            rfsuite.utils.log("Headspeed: " .. (rpm:value() or "-"), "info")
 
             local voltage = rfsuite.tasks.telemetry.getSensorSource("voltage")
-            rfsuite.utils.log("Voltage: " .. (voltage:value() or "-"),"info")
+            rfsuite.utils.log("Voltage: " .. (voltage:value() or "-"), "info")
 
-            -- perform an api request via msp
-            if (apiValue == nil) then
-                local API = rfsuite.tasks.msp.api.load("GOVERNOR_CONFIG","SCRIPTS:/rfsuite/tasks/msp/api/")
+            -- MSP API - this is and example how to use the API system to retrieve values from the fbl
+            -- note.  It is sometimes usefull to consider using this call: 
+            -- rfsuite.tasks.msp.mspQueue:isProcessed()
+            -- to check if there is anything in the queue - especially if your code is a bit more syncronous
+            if apiValue == nil then
+                local API = rfsuite.tasks.msp.api.load("GOVERNOR_CONFIG")
                 API.setCompleteHandler(function(self, buf)
                     local governorMode = API.readValue("gov_mode")
-                    rfsuite.utils.log("Api value: " .. governorMode, "info")
+                    rfsuite.utils.log("API Value: " .. governorMode, "info")
                     apiValue = governorMode
                 end)
                 API.setUUID("550e8400-e29b-41d4-a716-446655440000")
                 API.read()
-            end   
-
-
+            else
+                rfsuite.utils.log("API Value: " .. (apiValue or "-"), "info")
+            end
         else
-            rfsuite.utils.log("Init...","info")
+            rfsuite.utils.log("Init...", "info")
         end
 
-        lastPrintTime = currentTime -- Update the last rfsuite.utils.log time
+        lastPrintTime = currentTime
     end
-
-    return
 end
-
 
 local function init()
-	-- this is where we 'setup' the widget
-	
-	local key = "rfgbss"			        -- unique key - keep it less that 8 chars
-	local name = "Rotorflight API Demo"		-- name of widget
+    -- Register the widget
+    local key = "rfgbss"
+    local name = "Rotorflight API Demo"
 
-    system.registerWidget(
-        {
-            key = key,					-- unique project id
-            name = name,				-- name of widget
-            create = create,			-- function called when creating widget
-            configure = configure,		-- function called when configuring the widget (use ethos forms)
-            paint = paint,				-- function called when lcd.invalidate() is called
-            wakeup = wakeup,			-- function called as the main loop
-            read = read,				-- function called when starting widget and reading configuration params
-            write = write,				-- function called when saving values / changing values in the configuration menu
-			event = event,				-- function called when buttons or screen clips occur
-			menu = menu,				-- function called to add items to the menu
-			persistent = false,			-- true or false to make the widget carry values between sessions and models (not safe imho)
-        }
-    )
-
+    system.registerWidget({
+        key = key,
+        name = name,
+        create = create,
+        configure = configure,
+        paint = paint,
+        wakeup = wakeup,
+        read = read,
+        write = write,
+        event = event,
+        menu = menu,
+        persistent = false,
+    })
 end
 
-return {init = init}
+return { init = init }
