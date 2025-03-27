2183724637ef96a513b1aac77409e3234d3449eb
MO <52574424+Jiki-Mo@users.noreply.github.com>
2025-03-27 15:38:31 +0800

    Support for LED color selection for FLYROTOR ESC. (#515)
    Support for LED color selection for FLYROTOR ESC.

--- scripts/rfsuite/app/modules/esc_tools/mfg/flrtr/pages/esc_other.lua
+++ scripts/rfsuite/app/modules/esc_tools/mfg/flrtr/pages/esc_other.lua
@@ -15,16 +15,15 @@ local mspapi = {
         labels = {
         },
         fields = {
-            { t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.throttle_protocol"),             mspapi = 1, apikey = "throttle_protocol",  type = 1, mspapigt = 12.08},
-            { t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.telemetry_protocol"),            mspapi = 1, apikey = "telemetry_protocol", type = 1, mspapigt = 12.08},
-            --{ t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.led_color"),                     mspapi = 1, apikey = "led_color",          type = 1, mspapigt = 12.08},
-            { t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.motor_temp_sensor"),             mspapi = 1, apikey = "motor_temp_sensor",          type = 1, mspapigt = 12.08},
-            { t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.motor_temp"),                    mspapi = 1, apikey = "motor_temp",           mspapigt = 12.08},
-            { t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.battery_capacity"),              mspapi = 1, apikey = "battery_capacity",     mspapigt = 12.08},
+            { t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.throttle_protocol"),  mspapi = 1, apikey = "throttle_protocol",  type = 1,        mspapigt = 12.08 },
+            { t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.telemetry_protocol"), mspapi = 1, apikey = "telemetry_protocol", type = 1,        mspapigt = 12.08 },
+            { t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.led_color"),          mspapi = 1, apikey = "led_color_index",    type = 1,        mspapigt = 12.08 },
+            { t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.motor_temp_sensor"),  mspapi = 1, apikey = "motor_temp_sensor",  type = 1,        mspapigt = 12.08 },
+            { t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.motor_temp"),         mspapi = 1, apikey = "motor_temp",         mspapigt = 12.08 },
+            { t = rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.battery_capacity"),   mspapi = 1, apikey = "battery_capacity",   mspapigt = 12.08 },
         }
-    }                 
+    }
 }
-
 
 function postLoad()
     rfsuite.app.triggers.closeProgressLoader = true
@@ -36,28 +35,24 @@ local function onNavMenu(self)
 end
 
 local function event(widget, category, value, x, y)
-
     -- if close event detected go to section home page
     if category == EVT_CLOSE and value == 0 or value == 35 then
         if powercycleLoader then powercycleLoader:close() end
         rfsuite.app.ui.openPage(pidx, folder, "esc_tools/esc_tool.lua")
         return true
     end
-
-
 end
 
 return {
-    mspapi=mspapi,
+    mspapi = mspapi,
     eepromWrite = true,
     reboot = false,
     escinfo = escinfo,
     postLoad = postLoad,
-    simulatorResponse =  simulatorResponse,
-    navButtons = {menu = true, save = true, reload = true, tool = false, help = false},
+    simulatorResponse = simulatorResponse,
+    navButtons = { menu = true, save = true, reload = true, tool = false, help = false },
     onNavMenu = onNavMenu,
     event = event,
-    pageTitle = rfsuite.i18n.get("app.modules.esc_tools.name") .. " / " ..  rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.name") .. " / " .. rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.governor"),
+    pageTitle = rfsuite.i18n.get("app.modules.esc_tools.name") .. " / " .. rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.name") .. " / " .. rfsuite.i18n.get("app.modules.esc_tools.mfg.flrtr.other"),
     headerLine = rfsuite.escHeaderLineText
-
 }
