1c085f829fa939a20a1cee46cc977a2a8edf7058
Rob Thomson <rob@marotori.com>
2025-03-28 14:31:41 +0000

    Tilt Correction (#531)

--- scripts/rfsuite/app/modules/mixer/mixer.lua
+++ scripts/rfsuite/app/modules/mixer/mixer.lua
@@ -11,8 +11,8 @@ local mspapi = {
         fields = {
             {t = rfsuite.i18n.get("app.modules.mixer.geo_correction"),                  api = "MIXER_CONFIG:swash_geo_correction"},
             {t = rfsuite.i18n.get("app.modules.mixer.swash_pitch_limit"),               api = "MIXER_CONFIG:swash_pitch_limit"},
-            {t = rfsuite.i18n.get("app.modules.mixer.collective_tilt_correction_pos"),  api = "MIXER_CONFIG:collective_tilt_correction_pos", inline = 1, label = 1, apiversiongt = 12.08},
-            {t = rfsuite.i18n.get("app.modules.mixer.collective_tilt_correction_neg"),  api = "MIXER_CONFIG:collective_tilt_correction_neg", inline = 1, label = 2, apiversiongt = 12.08},
+            {t = rfsuite.i18n.get("app.modules.mixer.collective_tilt_correction_pos"),  api = "MIXER_CONFIG:collective_tilt_correction_pos", inline = 1, label = 1, apiversiongte = 12.08},
+            {t = rfsuite.i18n.get("app.modules.mixer.collective_tilt_correction_neg"),  api = "MIXER_CONFIG:collective_tilt_correction_neg", inline = 1, label = 2, apiversiongte = 12.08},
             {t = rfsuite.i18n.get("app.modules.mixer.swash_phase"),                     api = "MIXER_CONFIG:swash_phase"},
             {t = rfsuite.i18n.get("app.modules.mixer.swash_tta_precomp"),               api = "MIXER_CONFIG:swash_tta_precomp"},
             {t = rfsuite.i18n.get("app.modules.mixer.tail_motor_idle"),                 api = "MIXER_CONFIG:tail_motor_idle", enablefunction = function() return (rfsuite.session.tailMode >= 1) end},
