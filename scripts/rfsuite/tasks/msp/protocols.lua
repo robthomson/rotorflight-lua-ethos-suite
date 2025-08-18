fa8a8cbb46b5102387bd2bc02b3ca8ab969c3fdd
Rob Thomson <rob@marotori.com>
2025-08-18 08:23:02 +0100

    Use session for msp protocol detection (#1286)

--- scripts/rfsuite/tasks/msp/protocols.lua
+++ scripts/rfsuite/tasks/msp/protocols.lua
@@ -62,11 +62,14 @@ local supportedProtocols = {
 }
 
 --[[
-    Retrieves the communication protocol based on the availability of the "Rx RSSI1" source.
-    @return supportedProtocols.crsf if "Rx RSSI1" source is available, otherwise supportedProtocols.sport.
+    Retrieves the communication protocol based on the availability of the source.
 ]]
 function protocol.getProtocol()
-    if system.getSource("Rx RSSI1") ~= nil then return supportedProtocols.crsf end
+    if rfsuite.session and rfsuite.session.telemetryType then
+        if rfsuite.session.telemetryType == "crsf" then
+            return supportedProtocols.crsf
+        end
+    end
     return supportedProtocols.sport
 end
 
