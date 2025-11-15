b1cc75824c20d889b382ad926f6f399e30e37c15
Rob Thomson <rob@marotori.com>
2025-11-15 18:16:29 +0000

    Dashboard - Basic Timer Theme (#1525)

---
+++ src/rfsuite/widgets/dashboard/themes/timer/init.lua
@@ -0,0 +1,10 @@
+--[[
+  Copyright (C) 2025 Rotorflight Project
+  GPLv3 — https://www.gnu.org/licenses/gpl-3.0.en.html
+]] --
+
+local rfsuite = require("rfsuite")
+
+local init = {name = "Basic Timer", preflight = "preflight.lua", inflight = "inflight.lua", postflight = "postflight.lua", standalone = false}
+
+return init
