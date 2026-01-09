239d60b80a4a21029f4ae63261eff9f5bc4eda26
Rob Thomson <rob@marotori.com>
2026-01-09 17:11:30 +0000

    Esc motors config tool (#1599)
    * Throttle configuration
    
    * Telemetry working
    
    * Motor Config Tool

--- src/rfsuite/app/modules/esc_motors/esc_motors.lua
+++ src/rfsuite/app/modules/esc_motors/esc_motors.lua
@@ -5,40 +5,180 @@
 
 local rfsuite = require("rfsuite")
 
-local title = "Motor/ESC Features"
-local enableWakeup = false
-
-local apidata = {
-    api = {
-        [1] = 'MOTOR_CONFIG',
-        [2] = 'ESC_SENSOR_CONFIG'
-    },
-    formdata = {
-        labels = {
-            {t = "@i18n(app.modules.esc_motors.main_motor_ratio)@",    label = 1, inline_size = 15.5},
-            {t = "@i18n(app.modules.esc_motors.tail_motor_ratio)@",    label = 2, inline_size = 15.5}
-        },
-        fields = {
-            {t = "@i18n(app.modules.esc_motors.pinion)@",              api = "MOTOR_CONFIG:main_rotor_gear_ratio_0", label = 1, inline = 2},
-            {t = "@i18n(app.modules.esc_motors.main)@",                api = "MOTOR_CONFIG:main_rotor_gear_ratio_1", label = 1, inline = 1},
-            {t = "@i18n(app.modules.esc_motors.rear)@",                api = "MOTOR_CONFIG:tail_rotor_gear_ratio_0", label = 2, inline = 2},
-            {t = "@i18n(app.modules.esc_motors.front)@",               api = "MOTOR_CONFIG:tail_rotor_gear_ratio_1", label = 2, inline = 1},
-            {t = "@i18n(app.modules.esc_motors.motor_pole_count)@",    api = "MOTOR_CONFIG:motor_pole_count_0"},
-            {t = "@i18n(app.modules.esc_motors.mincommand)@",          api = "MOTOR_CONFIG:mincommand"},
-            {t = "@i18n(app.modules.esc_motors.min_throttle)@",        api = "MOTOR_CONFIG:minthrottle"},
-            {t = "@i18n(app.modules.esc_motors.max_throttle)@",        api = "MOTOR_CONFIG:maxthrottle"},
-            {t = "@i18n(app.modules.esc_motors.voltage_correction)@",  api = "ESC_SENSOR_CONFIG:voltage_correction",    apiversion = 12.08},
-            {t = "@i18n(app.modules.esc_motors.current_correction)@",  api = "ESC_SENSOR_CONFIG:current_correction",    apiversion = 12.08},
-            {t = "@i18n(app.modules.esc_motors.consumption_correction)@", api = "ESC_SENSOR_CONFIG:consumption_correction", apiversion = 12.08}
-        }
+local  S_PAGES ={
+        [1] = { name = "@i18n(app.modules.esc_motors.throttle)@", script = "throttle.lua", image = "throttle.png" },
+        [2] = { name = "@i18n(app.modules.esc_motors.telemetry)@", script = "telemetry.lua", image = "telemetry.png"},    
+        [3] = { name = "@i18n(app.modules.esc_motors.rpm)@", script = "rpm.lua", image = "rpm.png"},
     }
-}
 
-local function postLoad(self)
+local enableWakeup = false
+local prevConnectedState = nil
+local initTime = os.clock()
+local esc_motorsCompatibilityStatus = false
+
+
+
+local function openPage(pidx, title, script)
+
+    rfsuite.tasks.msp.protocol.mspIntervalOveride = nil
+
+    rfsuite.app.triggers.isReady = false
+    rfsuite.app.uiState = rfsuite.app.uiStatus.mainMenu
+
+    form.clear()
+
+    rfsuite.app.lastIdx = idx
+    rfsuite.app.lastTitle = title
+    rfsuite.app.lastScript = script
+
+    for i in pairs(rfsuite.app.gfx_buttons) do if i ~= "esc_motors" then rfsuite.app.gfx_buttons[i] = nil end end
+
+    if rfsuite.preferences.general.iconsize == nil or rfsuite.preferences.general.iconsize == "" then
+        rfsuite.preferences.general.iconsize = 1
+    else
+        rfsuite.preferences.general.iconsize = tonumber(rfsuite.preferences.general.iconsize)
+    end
+
+    local w, h = lcd.getWindowSize()
+    local windowWidth = w
+    local windowHeight = h
+    local padding = rfsuite.app.radio.buttonPadding
+
+    local sc
+    local panel
+
+    local buttonW = 100
+    local x = windowWidth - buttonW - 10
+
+    rfsuite.app.ui.fieldHeader("@i18n(app.modules.esc_motors.name)@")
+
+    local buttonW
+    local buttonH
+    local padding
+    local numPerRow
+
+    if rfsuite.preferences.general.iconsize == 0 then
+        padding = rfsuite.app.radio.buttonPaddingSmall
+        buttonW = (rfsuite.app.lcdWidth - padding) / rfsuite.app.radio.buttonsPerRow - padding
+        buttonH = rfsuite.app.radio.navbuttonHeight
+        numPerRow = rfsuite.app.radio.buttonsPerRow
+    end
+
+    if rfsuite.preferences.general.iconsize == 1 then
+
+        padding = rfsuite.app.radio.buttonPaddingSmall
+        buttonW = rfsuite.app.radio.buttonWidthSmall
+        buttonH = rfsuite.app.radio.buttonHeightSmall
+        numPerRow = rfsuite.app.radio.buttonsPerRowSmall
+    end
+
+    if rfsuite.preferences.general.iconsize == 2 then
+
+        padding = rfsuite.app.radio.buttonPadding
+        buttonW = rfsuite.app.radio.buttonWidth
+        buttonH = rfsuite.app.radio.buttonHeight
+        numPerRow = rfsuite.app.radio.buttonsPerRow
+    end
+
+    if rfsuite.app.gfx_buttons["esc_motors"] == nil then rfsuite.app.gfx_buttons["esc_motors"] = {} end
+    if rfsuite.preferences.menulastselected["esc_motors"] == nil then rfsuite.preferences.menulastselected["esc_motors"] = 1 end
+
+    local Menu = assert(loadfile("app/modules/" .. script))()
+    local pages = S_PAGES
+    local lc = 0
+    local bx = 0
+    local y = 0
+
+    for pidx, pvalue in ipairs(S_PAGES) do
+
+        if lc == 0 then
+            if rfsuite.preferences.general.iconsize == 0 then y = form.height() + rfsuite.app.radio.buttonPaddingSmall end
+            if rfsuite.preferences.general.iconsize == 1 then y = form.height() + rfsuite.app.radio.buttonPaddingSmall end
+            if rfsuite.preferences.general.iconsize == 2 then y = form.height() + rfsuite.app.radio.buttonPadding end
+        end
+
+        if lc >= 0 then bx = (buttonW + padding) * lc end
+
+        if rfsuite.preferences.general.iconsize ~= 0 then
+            if rfsuite.app.gfx_buttons["esc_motors"][pidx] == nil then rfsuite.app.gfx_buttons["esc_motors"][pidx] = lcd.loadMask("app/modules/esc_motors/gfx/" .. pvalue.image) end
+        else
+            rfsuite.app.gfx_buttons["esc_motors"][pidx] = nil
+        end
+
+        rfsuite.app.formFields[pidx] = form.addButton(line, {x = bx, y = y, w = buttonW, h = buttonH}, {
+            text = pvalue.name,
+            icon = rfsuite.app.gfx_buttons["esc_motors"][pidx],
+            options = FONT_S,
+            paint = function() end,
+            press = function()
+                rfsuite.preferences.menulastselected["esc_motors"] = pidx
+                rfsuite.app.ui.progressDisplay(nil,nil,false)
+                local name = "@i18n(app.modules.esc_motors.name)@" .. " / " .. pvalue.name
+                rfsuite.app.ui.openPage(pidx, name, "esc_motors/tools/" .. pvalue.script)
+            end
+        })
+
+        if pvalue.disabled == true then rfsuite.app.formFields[pidx]:enable(false) end
+
+        if pvalue.apiversion ~= nil then
+            local apiVersionSupported = rfsuite.utils.apiVersionCompare(">=", pvalue.apiversion)
+            if not apiVersionSupported then
+                rfsuite.app.formFields[pidx]:enable(false)
+            end
+        end
+
+        local currState = (rfsuite.session.isConnected and rfsuite.session.mcu_id) and true or false
+
+        if rfsuite.preferences.menulastselected["esc_motors"] == pidx then rfsuite.app.formFields[pidx]:focus() end
+
+        lc = lc + 1
+
+        if lc == numPerRow then lc = 0 end
+
+    end
+
     rfsuite.app.triggers.closeProgressLoader = true
+
     enableWakeup = true
+
+    return
 end
 
-local function wakeup() if enableWakeup == true then end end
+local function event(widget, category, value, x, y)
 
-return {apidata = apidata, reboot = false, eepromWrite = true, title = title, event = event, wakeup = wakeup, postLoad = postLoad}
+    if category == EVT_CLOSE and value == 0 or value == 35 then
+        rfsuite.app.ui.openMainMenuSub(rfsuite.app.lastMenu)
+        return true
+    end
+end
+
+local function onNavMenu()
+    rfsuite.app.ui.progressDisplay()
+    rfsuite.app.ui.openMainMenuSub('hardware')
+    return true
+end
+
+
+local function wakeup()
+    if not enableWakeup then return end
+
+    if os.clock() - initTime < 0.25 then return end
+
+    local currState = (rfsuite.session.isConnected and rfsuite.session.mcu_id) and true or false
+
+    if currState ~= prevConnectedState then
+
+        --rfsuite.app.formFields[2]:enable(currState)
+
+        if not currState then rfsuite.app.formNavigationFields['menu']:focus() end
+
+        prevConnectedState = currState
+    end
+
+
+
+end
+
+rfsuite.app.uiState = rfsuite.app.uiStatus.pages
+
+return {pages = pages, openPage = openPage, onNavMenu = onNavMenu, event = event, wakeup = wakeup, API = {}, navButtons = {menu = true, save = false, reload = false, tool = false, help = false}}
---
+++ src/rfsuite/app/modules/esc_motors/tools/throttle.lua
@@ -0,0 +1,137 @@
+--[[
+  Copyright (C) 2025 Rotorflight Project
+  GPLv3 — https://www.gnu.org/licenses/gpl-3.0.en.html
+]] --
+
+local rfsuite = require("rfsuite")
+
+local enableWakeup = false
+local formFields = rfsuite.app.formFields
+
+
+local FIELDKEY = {
+    PROTOCOL = 1,
+    PWM_RATE = 2,
+    MINCOMMAND = 3,
+    MINTHROTTLE = 4,
+    MAXTHROTTLE = 5,
+    UNSYNCED = 6
+}
+
+local apidata = {
+    api = {
+        [1] = 'MOTOR_CONFIG',
+    },
+    formdata = {
+        labels = {
+        },
+        fields = {
+            [FIELDKEY.PROTOCOL] = {t = "@i18n(app.modules.esc_motors.throttle_protocol)@",      api = "MOTOR_CONFIG:motor_pwm_protocol", type = 1},
+            [FIELDKEY.PWM_RATE] = {t = "@i18n(app.modules.esc_motors.motor_pwm_rate)@",         api = "MOTOR_CONFIG:motor_pwm_rate"},
+            [FIELDKEY.MINCOMMAND] = {t = "@i18n(app.modules.esc_motors.mincommand)@",           api = "MOTOR_CONFIG:mincommand"},
+            [FIELDKEY.MINTHROTTLE] = {t = "@i18n(app.modules.esc_motors.min_throttle)@",        api = "MOTOR_CONFIG:minthrottle"},
+            [FIELDKEY.MAXTHROTTLE] = {t = "@i18n(app.modules.esc_motors.max_throttle)@",        api = "MOTOR_CONFIG:maxthrottle"},
+            [FIELDKEY.UNSYNCED] = {t = "@i18n(app.modules.esc_motors.unsynced)@",               api = "MOTOR_CONFIG:use_unsynced_pwm", type = 1},
+        }
+    }
+}
+
+local function postLoad(self)
+    rfsuite.app.triggers.closeProgressLoader = true
+    enableWakeup = true
+end
+
+local function wakeup() 
+    if enableWakeup == true then end 
+
+    local protocolValue = rfsuite.app.Page.apidata.formdata.fields[FIELDKEY.PROTOCOL].value
+    if protocolValue == nil then
+        protocolValue = 0
+    else
+        protocolValue = math.floor(protocolValue)
+    end
+
+    local unsyncedValue
+    unsyncedValue = rfsuite.app.Page.apidata.formdata.fields[FIELDKEY.UNSYNCED].value
+
+    -- if using things like dshot this comes back as nil - and that mangles the form drop downs so reset it to off
+    if unsyncedValue == nil then
+        rfsuite.app.Page.apidata.formdata.fields[FIELDKEY.UNSYNCED].value = 0
+    end
+
+    if protocolValue == 0 then  -- PWM          (PWM_RATE, MINCOMMAND, MINTHROTTLE, MAXTHROTTLE)
+        formFields[FIELDKEY.PWM_RATE]:enable(true)
+        formFields[FIELDKEY.MINCOMMAND]:enable(true)
+        formFields[FIELDKEY.MINTHROTTLE]:enable(true)
+        formFields[FIELDKEY.MAXTHROTTLE]:enable(true)
+        formFields[FIELDKEY.UNSYNCED]:enable(false)
+    elseif(protocolValue == 1)   then  -- ONESHOT125  (PROTOCOL , PWM_RATE, MINCOMMAND, MINTHROTTLE, MAXTHROTTLE, UNSYNCED)
+        formFields[FIELDKEY.PWM_RATE]:enable(true)
+        formFields[FIELDKEY.MINCOMMAND]:enable(true)
+        formFields[FIELDKEY.MINTHROTTLE]:enable(true)
+        formFields[FIELDKEY.MAXTHROTTLE]:enable(true)
+        formFields[FIELDKEY.UNSYNCED]:enable(true)
+    elseif(protocolValue == 2) then  -- ONESHOT 42 (PROTOCOL , PWM_RATE, MINCOMMAND, MINTHROTTLE, MAXTHROTTLE, UNSYNCED)
+        formFields[FIELDKEY.PWM_RATE]:enable(true)
+        formFields[FIELDKEY.MINCOMMAND]:enable(true)
+        formFields[FIELDKEY.MINTHROTTLE]:enable(true)
+        formFields[FIELDKEY.MAXTHROTTLE]:enable(true)
+        formFields[FIELDKEY.UNSYNCED]:enable(true)
+    elseif(protocolValue == 3) then  -- MULTISHOT (PROTOCOL , PWM_RATE, MINCOMMAND, MINTHROTTLE, MAXTHROTTLE, UNSYNCED)
+        formFields[FIELDKEY.PWM_RATE]:enable(true)
+        formFields[FIELDKEY.MINCOMMAND]:enable(true)
+        formFields[FIELDKEY.MINTHROTTLE]:enable(true)
+        formFields[FIELDKEY.MAXTHROTTLE]:enable(true)
+        formFields[FIELDKEY.UNSYNCED]:enable(true)
+    elseif(protocolValue == 4) then  -- BRUSHED (PROTOCOL , PWM_RATE, MINCOMMAND, MINTHROTTLE, MAXTHROTTLE, UNSYNCED)
+        formFields[FIELDKEY.PWM_RATE]:enable(true)
+        formFields[FIELDKEY.MINCOMMAND]:enable(true)
+        formFields[FIELDKEY.MINTHROTTLE]:enable(true)
+        formFields[FIELDKEY.MAXTHROTTLE]:enable(true)
+        formFields[FIELDKEY.UNSYNCED]:enable(true)        
+    elseif(protocolValue == 5) then  -- DSHOT150
+        formFields[FIELDKEY.PWM_RATE]:enable(false)
+        formFields[FIELDKEY.MINCOMMAND]:enable(false)
+        formFields[FIELDKEY.MINTHROTTLE]:enable(false)
+        formFields[FIELDKEY.MAXTHROTTLE]:enable(false)
+        formFields[FIELDKEY.UNSYNCED]:enable(false)
+    elseif(protocolValue == 6) then  -- DSHOT300 
+        formFields[FIELDKEY.PWM_RATE]:enable(false)
+        formFields[FIELDKEY.MINCOMMAND]:enable(false)
+        formFields[FIELDKEY.MINTHROTTLE]:enable(false)
+        formFields[FIELDKEY.MAXTHROTTLE]:enable(false)
+        formFields[FIELDKEY.UNSYNCED]:enable(false)
+    elseif(protocolValue == 7) then  -- DSHOT600
+        formFields[FIELDKEY.PWM_RATE]:enable(false)
+        formFields[FIELDKEY.MINCOMMAND]:enable(false)
+        formFields[FIELDKEY.MINTHROTTLE]:enable(false)
+        formFields[FIELDKEY.MAXTHROTTLE]:enable(false)
+        formFields[FIELDKEY.UNSYNCED]:enable(false)
+    elseif(protocolValue == 8) then  -- PROSHOT
+        formFields[FIELDKEY.PWM_RATE]:enable(false)
+        formFields[FIELDKEY.MINCOMMAND]:enable(false)
+        formFields[FIELDKEY.MINTHROTTLE]:enable(false)
+        formFields[FIELDKEY.MAXTHROTTLE]:enable(false)
+        formFields[FIELDKEY.UNSYNCED]:enable(false)        
+    elseif(protocolValue == 9 and rfsuite.utils.apiVersionCompare(">=", "12.07")) then  -- CASTLE
+        formFields[FIELDKEY.PWM_RATE]:enable(true)
+        formFields[FIELDKEY.MINCOMMAND]:enable(true)
+        formFields[FIELDKEY.MINTHROTTLE]:enable(true)
+        formFields[FIELDKEY.MAXTHROTTLE]:enable(true)
+        formFields[FIELDKEY.UNSYNCED]:enable(false)
+    else  -- DISABLED
+        formFields[FIELDKEY.PWM_RATE]:enable(false)
+        formFields[FIELDKEY.MINCOMMAND]:enable(false)
+        formFields[FIELDKEY.MINTHROTTLE]:enable(false)
+        formFields[FIELDKEY.MAXTHROTTLE]:enable(false)
+        formFields[FIELDKEY.UNSYNCED]:enable(false)
+    end
+
+        
+end
+
+local function onNavMenu(self)
+    rfsuite.app.ui.openPage(pidx, title, "esc_motors/esc_motors.lua")
+end
+
+return {apidata = apidata, reboot = true, eepromWrite = true, title = title, event = event, wakeup = wakeup, postLoad = postLoad, onNavMenu = onNavMenu}
