0fad34efca653924ffde667f1cf25cb2f0fd94b4
Rob Thomson <rob@marotori.com>
2026-02-03 09:37:02 +0000

    Ensure rfsuite.app.guiIsRunning is always false on first load (#1717)

--- src/rfsuite/app/app.lua
+++ src/rfsuite/app/app.lua
@@ -13,6 +13,9 @@ local compile = loadfile
 local arg = {...}
 local config = arg[1]
 
+-- Make sure this is set - even if not initialised
+app.guiIsRunning = false
+
 function app.paint()
     if app.Page and app.Page.paint then app.Page.paint(app.Page) end
 
