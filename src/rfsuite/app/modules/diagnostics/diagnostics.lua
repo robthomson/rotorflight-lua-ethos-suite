c24bbd196c4a41892b2202b23359bbe064abf1ae
Rob Thomson <rob@marotori.com>
2026-02-15 12:38:48 +0000

    Sensors Tool  (#1878)

--- src/rfsuite/app/modules/diagnostics/diagnostics.lua
+++ src/rfsuite/app/modules/diagnostics/diagnostics.lua
@@ -10,6 +10,7 @@ local lcd = lcd
 
 local S_PAGES = {
     {name = "@i18n(app.modules.rfstatus.name)@", script = "rfstatus.lua", image = "rfstatus.png", bgtask = false, offline = false}, {name = "@i18n(app.modules.validate_sensors.name)@", script = "sensors.lua", image = "sensors.png", bgtask = true, offline = true},
+    {name = "FBL Sensors", script = "fblsensors.lua", image = "fblsensors.png", bgtask = true, offline = true},
     {name = "@i18n(app.modules.fblstatus.name)@", script = "fblstatus.lua", image = "fblstatus.png", bgtask = true, offline = true}, {name = "@i18n(app.modules.info.name)@", script = "info.lua", image = "info.png", bgtask = true, offline = true},
 }
 
