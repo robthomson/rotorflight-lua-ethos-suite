437e2c051a15d88ce0e0cbb47db3603de8a8d155
Rob Thomson <rob@marotori.com>
2026-02-15 21:13:00 +0000

    Configuration Module (#1890)

---
+++ src/rfsuite/app/modules/configuration/help.lua
@@ -0,0 +1,16 @@
+--[[
+  Copyright (C) 2026 Rotorflight Project
+  GPLv3 -- https://www.gnu.org/licenses/gpl-3.0.en.html
+]] --
+
+local data = {}
+
+data.help = {}
+data.help.default = {
+    "@i18n(app.modules.configuration.help_p1)@",
+    "@i18n(app.modules.configuration.help_p2)@"
+}
+
+data.fields = {}
+
+return data
