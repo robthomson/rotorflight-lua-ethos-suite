51e7990d25c5a49c868637561c9759181135f38e
Rob Thomson <rob@marotori.com>
2026-02-17 11:48:35 +0000

    Remove some legacy (orphaned) modules (#1912)

--- src/rfsuite/app/modules/msp_exp/init.lua
+++
@@ -1,9 +0,0 @@
---[[
-  Copyright (C) 2025 Rotorflight Project
-  GPLv3 — https://www.gnu.org/licenses/gpl-3.0.en.html
-]] --
-
-
-local init = {title = "@i18n(app.modules.msp_exp.name)@", section = "tools", script = "msp_exp.lua", image = "msp_exp.png", order = 100, developer = true, ethosversion = {1, 6, 2}, disable = true}
-
-return init
--- src/rfsuite/app/modules/tailrotor/init.lua
+++
@@ -1,8 +0,0 @@
---[[
-  Copyright (C) 2025 Rotorflight Project
-  GPLv3 — https://www.gnu.org/licenses/gpl-3.0.en.html
-]] --
-
-local init = {title = "@i18n(app.modules.profile_tailrotor.name)@", section = "main", script = "tailrotor.lua", image = "tailrotor.png", order = 2, ethosversion = {1, 6, 2}}
-
-return init
