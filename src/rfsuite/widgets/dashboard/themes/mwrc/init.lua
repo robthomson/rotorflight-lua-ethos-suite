935b2e0973319c153418988f1652203daa72d096
Michael Wallace <mbwallace1390@gmail.com>
2026-06-29 10:47:33 -0500

    Add files via upload

---
+++ src/rfsuite/widgets/dashboard/themes/mwrc/init.lua
@@ -0,0 +1,19 @@
+--[[
+  Copyright (C) 2025 Rotorflight Project
+  GPLv3 — https://www.gnu.org/licenses/gpl-3.0.en.html
+]] --
+
+local rfsuite = require("rfsuite")
+
+local init = {
+    name = "mwrc",
+    preflight = "preflight.lua",
+    inflight = "inflight.lua",
+    postflight = "postflight.lua",
+    configure = "configure.lua",
+    standalone = false,
+    minResolution = {x = 784, y = 294},
+    logo = {dark = "gfx/rfsuite-dark.png", light = "gfx/rfsuite-light.png"}
+}
+
+return init
