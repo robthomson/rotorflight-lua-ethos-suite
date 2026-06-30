fa1838c555f47d2ca8120ff533778ca141e579cb
Michael Wallace <mbwallace1390@gmail.com>
2026-06-30 00:08:59 -0500

    Prepare MWRC theme v17

--- src/rfsuite/widgets/dashboard/themes/mwrc/init.lua
+++ src/rfsuite/widgets/dashboard/themes/mwrc/init.lua
@@ -3,8 +3,6 @@
   GPLv3 — https://www.gnu.org/licenses/gpl-3.0.en.html
 ]] --
 
-local rfsuite = require("rfsuite")
-
 local init = {
     name = "mwrc",
     preflight = "preflight.lua",
@@ -12,8 +10,7 @@ local init = {
     postflight = "postflight.lua",
     configure = "configure.lua",
     standalone = false,
-    minResolution = {x = 784, y = 294},
-    logo = {dark = "gfx/rfsuite-dark.png", light = "gfx/rfsuite-light.png"}
+    minResolution = {x = 784, y = 294}
 }
 
 return init
